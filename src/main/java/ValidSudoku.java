80ccb67ec06d2f0eea7e5de0b14098c961f07040
Chris-QingYuan <chris453442895@gmail.com>
2018-02-01 02:00:17 -0500

    update

--- src/main/java/ValidSudoku.java
+++ src/main/java/ValidSudoku.java
@@ -6,6 +6,8 @@ public class ValidSudoku{
     static final List<Character> oneToNine = Arrays.asList('1','2','3','4','5','6','7','8','9');
 
     public static void main(String[] args){
+
+        
         System.out.println(" ");
 
     }
