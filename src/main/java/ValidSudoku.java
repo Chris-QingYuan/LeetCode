7cd1d21a93f624996b25b399507c4ac19f106a11
Chris-QingYuan <chris453442895@gmail.com>
2018-02-01 02:33:13 -0500

    thi-rd

--- src/main/java/ValidSudoku.java
+++ src/main/java/ValidSudoku.java
@@ -10,8 +10,8 @@ public class ValidSudoku{
         //not much
         System.out.println(" ");
         //another one
-        //second dada
-        //thi
+        //second
+        //third
 
     }
 
